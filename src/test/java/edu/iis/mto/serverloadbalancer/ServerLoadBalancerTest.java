84ec8e87f3e997b5df91dff7834aaf6159247ced
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-20 10:19:40 +0200

    Test 1 - refactor
    * extract method for checking double equality in 
      CurrentLoadPercentageMatcher class
    * extract magic number 0.01 to constant
    * move factory method "server" from test to ServerBuilder class
    * move factory method "hasLoadPercentageOf" to 
      CurrentLoadPercentageMatcher class

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,6 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -8,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -26,10 +27,7 @@ public class ServerLoadBalancerTest {
 		assertThat( theServer , hasLoadPercentageOf( 0.0d ) );
 	}
 
-	private Matcher<? super Server> hasLoadPercentageOf(double loadPercentage) {
-		return new CurrentLoadPercentageMatcher( loadPercentage );
-	}
-
+	
 	private void balance(List<Server> servers,
 			List<Vm> vms) {
 		new ServerLoadBalancer().balance( servers , vms );
@@ -45,10 +43,6 @@ public class ServerLoadBalancerTest {
 
 	private Server a(ServerBuilder builder) {
 		return builder.build();
-	}
-
-	private ServerBuilder server() {
-		return new ServerBuilder();
 	}
 	
 }
