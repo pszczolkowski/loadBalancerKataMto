588b854f6587bff1e30710e004466419214bbdca
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-27 18:28:49 +0200

    Zmiana testu na parametryzowany
    Test został zmodyfikowany, by wykonywać się dla różnych parametrów.

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -5,18 +5,38 @@ import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith( Parameterized.class )
 public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
+	
+	private int size;
+
+	public ServerLoadBalancerParametrizedTest(int size) {
+		this.size = size;
+	}
+
+	@Parameters
+    public static Collection< Object[] > data() {
+        return Arrays.asList( new Object[][] {
+        		{ 1 } , { 2 } , { 3 } , { 10 } , { 11 } , { 100 }
+        });
+    }
+	
 	@Test
 	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
+		Server theServer = a(server().withCapacity( size ));
+		Vm theVm = a(vm().ofSize( size ));
 		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
 		assertThat(theServer, hasLoadPercentageOf(100.0d));
 		assertThat("the server should contain vm", theServer.contains(theVm));
-	}
-	
+	}	
 	
 }
