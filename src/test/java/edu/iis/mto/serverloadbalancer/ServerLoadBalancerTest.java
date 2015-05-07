45edb0ff95b854314b96c88ad19945dedaedc2fb
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-05-07 22:03:32 +0200

    test 7 - red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -86,6 +86,25 @@ public class ServerLoadBalancerTest {
 		
 		assertFalse( "the more loaded server should not contain the vm" ,  theServer.contains( theVm ) );
 	}
+	
+	@Test
+	public void balanceServersAnsVms(){
+        Server server1 = a(server().withCapacity(4));
+        Server server2 = a(server().withCapacity(6));
+        
+        Vm vm1 = a(vm().ofSize(1));
+        Vm vm2 = a(vm().ofSize(4));
+        Vm vm3 = a(vm().ofSize(2));
+        
+        balance(aListOfServersWith(server1, server2), aListOfVmsWith(vm1, vm2, vm3));
+        
+        assertThat("The server 1 should contain the vm 1", server1.contains(vm1));
+        assertThat("The server 2 should contain the vm 2", server2.contains(vm2));
+        assertThat("The server 1 should contain the vm 3", server1.contains(vm3));
+        
+        assertThat(server1, hasLoadPercentageOf(75.0d));
+        assertThat(server2, hasLoadPercentageOf(66.66d));
+	}
 
 
 	private List<Vm> aListOfVmsWith(Vm... vms) {
