421dd7ef4d0c1a02b61330a9e0c5fc0c707e7157
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-05-08 21:12:36 +0200

    test 1 - green

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -3,9 +3,12 @@ package edu.iis.mto.serverloadbalancer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
-import org.junit.Assert;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -16,11 +19,36 @@ public class ServerLoadBalancerTest {
 
 	@Test
 	public void balancingAServerWithOneSlot_noVms_serverShouldStayEmpty(){
-		Server theServer = a( server().withCapacity( 1 ) );
 		
-		balance( aListOfServersWith( theServer ) , anEmptyListOfVms() );;
+		Server theServer = a( server().withCapacity( 1 ) );
+		balance( aListOfServersWith( theServer ) , anEmptyListOfVms() );
 		
 		assertThat( theServer , hasLoadPercentageOf( 0.0d ));
+	}
+
+	private Matcher<? super Server> hasLoadPercentageOf(double loadPercentage) {
+		return new CurrentLoadPercentageMatcher( loadPercentage );
+	}
+
+	private void balance(List<Server> servers,
+			List<Vm> vms) {
+		new ServerLoadBalancer().balance( servers , vms );
+	}
+
+	private List< Vm > anEmptyListOfVms() {
+		return new ArrayList< Vm >();
+	}
+
+	private List< Server > aListOfServersWith(Server... servers) {
+		return Arrays.asList( servers );
+	}
+
+	private Server a(ServerBuilder builder) {
+		return builder.build();
+	}
+
+	private ServerBuilder server() {
+		return new ServerBuilder();
 	}
 
 }
