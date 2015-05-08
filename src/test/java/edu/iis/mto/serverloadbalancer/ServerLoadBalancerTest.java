41e21bd9693dec9e87a93eb4eb8c82fbbabf327b
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-05-08 21:03:08 +0200

    test 1 - red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -3,7 +3,9 @@ package edu.iis.mto.serverloadbalancer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
@@ -12,5 +14,13 @@ public class ServerLoadBalancerTest {
 		assertThat(true, equalTo(true));
 	}
 
+	@Test
+	public void balancingAServerWithOneSlot_noVms_serverShouldStayEmpty(){
+		Server theServer = a( server().withCapacity( 1 ) );
+		
+		balance( aListOfServersWith( theServer ) , anEmptyListOfVms() );;
+		
+		assertThat( theServer , hasLoadPercentageOf( 0.0d ));
+	}
 
 }
