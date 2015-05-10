2bbda322eca0f47f220d81c5262b2a4224a2f5ce
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-05-10 10:53:10 +0200

    test 6 - green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -1,12 +1,20 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServerLoadBalancer {
 
 	public void balance(List<Server> servers, List<Vm> vms) {
 		for( Vm vm: vms ){
-			Server leastLoadedServer = findLeastLoadedServerIn(servers);
+			List< Server > serversWithEnoughCapacity = new ArrayList< Server >();
+			for( Server server : servers ){
+				if( server.canFit( vm ) ){
+					serversWithEnoughCapacity.add( server );
+				}
+			}
+			
+			Server leastLoadedServer = findLeastLoadedServerIn( serversWithEnoughCapacity );
 			
 			if( leastLoadedServer != null )
 				leastLoadedServer.addVm( vm );
