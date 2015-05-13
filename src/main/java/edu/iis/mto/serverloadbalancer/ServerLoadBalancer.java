d3ade3f1e1dba16a6f602a1f867f1697f0976e4b
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-05-13 19:49:20 +0200

    test 5 - refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,16 +4,21 @@ public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
 		for( Vm vm: vms ){
-			Server leastLoadedServer = null;
-			for( Server server : servers ){
-				if( leastLoadedServer == null || server.currentLoadPercentage <= leastLoadedServer.currentLoadPercentage ){
-					leastLoadedServer = server;
-				}
-			}
+			Server leastLoadedServer = findLeastLoadedServerIn(servers);
 			
 			if( leastLoadedServer != null )
 				leastLoadedServer.addVm( vm );
 		}
+	}
+
+	private Server findLeastLoadedServerIn(Server[] servers) {
+		Server leastLoadedServer = null;
+		for( Server server : servers ){
+			if( leastLoadedServer == null || server.currentLoadPercentage <= leastLoadedServer.currentLoadPercentage ){
+				leastLoadedServer = server;
+			}
+		}
+		return leastLoadedServer;
 	}
 
 }
