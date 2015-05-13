2a6b6a9f6891993d1332b673ccc3b7abbea5040c
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-05-13 12:34:49 +0200

    test 3 - red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -34,6 +34,17 @@ public class ServerLoadBalancerTest {
 		assertThat( theServer , hasCurrentLoadPercentageOf( 100.0d ));
 		assertThat( "the server should contain the vm" , theServer.contains( theVm ));
 	}
+	
+	@Test
+	public void balancingAServerWithTenSlotsCapacity_andOneSlotVm_serverShouldBeFilledInTenPercent(){
+		Server theServer = a( server().withCapacity( 10 ) );
+		Vm theVm = a( vm().ofSize( 1 ) );
+		
+		balance( aListOfServersWith( theServer ) , aListOfVmsWith( theVm ) );
+		
+		assertThat( theServer , hasCurrentLoadPercentageOf( 10.0d ));
+		assertThat( "the server should contain the vm" , theServer.contains( theVm ));
+	}
 
 
 	private Vm[] aListOfVmsWith(Vm theVm) {
